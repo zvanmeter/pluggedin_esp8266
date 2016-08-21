80e674de54ff5277350df1a97abd6af4deaadf59
Zach VanMeter <zvanmeter@gmail.com>
2016-08-21 15:09:06 -0500

    Print basic message to verify serial connectivity

--- hello_world/hello_world.ino
+++ hello_world/hello_world.ino
@@ -4,12 +4,16 @@ const int LED_FLASH_MILLISECONDS = 500;
 void setup()
 {
   pinMode(BOARD_LED_PIN, OUTPUT);
+  Serial.begin(9600);
+  Serial.println("Hello, world!");
 }
 
 void loop()
 {
+  Serial.println("Turning LED on.");
   digitalWrite(BOARD_LED_PIN, HIGH);
   delay(LED_FLASH_MILLISECONDS);
+  Serial.println("Turning LED off.");
   digitalWrite(BOARD_LED_PIN, LOW);
   delay(LED_FLASH_MILLISECONDS);
 }
